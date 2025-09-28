dae2c8ee112193caac3535c7f392d5ecbb716820
tdnguyen98 <theo-nguy@hotmail.com>
2025-09-28 11:08:02 +0200

    switch to testnet

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -10,7 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <SuiClientProvider networks={networkConfig} defaultNetwork="devnet">
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
         <WalletProvider autoConnect>
           {children}
         </WalletProvider>
